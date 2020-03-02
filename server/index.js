ff71d88f09379ceff44d89218543bd82cefb5221
Julian Taylor <juliantaylor14@gmail.com>
2020-03-02 23:44:20 +0000

    remove file based db

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
-const { models, db } = require("./db");
+// const { models, db } = require("./db");
+
+const { createStore } = require("./utils");
+
+const TodoListsAPI = require("./datasources/todoLists");
+const TodoItemsAPI = require("./datasources/todoItems");
+
+// creates a sequelize connection once. NOT for every request
+const store = createStore();
+
+// set up any dataSources our resolvers need
+const dataSources = () => ({
+	todoListsAPI: new TodoListsAPI({ store }),
+	todoItemsAPI: new TodoItemsAPI({ store })
+});
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	context() {
-		return { models, db };
-	}
+	dataSources
+	// context() {
+	// 	return { models, db };
+	// }
 });
 
 server.listen().then(({ url }) => {
