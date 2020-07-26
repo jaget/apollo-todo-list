933bd2dd0f1c743d4a8750fc6a056da41f0b30ad
jaget <juliantaylor14@googlemail.com>
2020-07-26 22:41:43 +0100

    Implement subscriptions

--- client/src/pages/TodoItems.jsx
+++ client/src/pages/TodoItems.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import gql from "graphql-tag";
 import TodoItemBox from "../components/TodoItemBox";
 import NewTodoItem from "../components/NewTodoItem";
@@ -6,86 +6,125 @@ import { useQuery, useMutation } from "@apollo/react-hooks";
 import Loader from "../components/Loader";
 
 const TODOITEM_DETAILS = gql`
-	fragment TodoItemDetails on TodoItem {
-		id
-		label
-		isCompleted
-	}
+  fragment TodoItemDetails on TodoItem {
+    id
+    label
+    isCompleted
+  }
 `;
 
 const GET_TODOITEMS = gql`
-	query todoItemsList {
-		todoItems {
-			...TodoItemDetails
-		}
-	}
-	${TODOITEM_DETAILS}
+  query todoItemsList {
+    todoItems {
+      ...TodoItemDetails
+    }
+  }
+  ${TODOITEM_DETAILS}
 `;
 
 const CREATE_TODOITEM = gql`
-	mutation CreateTodoItem($input: NewTodoItemInput!) {
-		addTodoItem(input: $input) {
-			...TodoItemDetails
-		}
-	}
-	${TODOITEM_DETAILS}
+  mutation CreateTodoItem($input: NewTodoItemInput!) {
+    addTodoItem(input: $input) {
+      ...TodoItemDetails
+    }
+  }
+  ${TODOITEM_DETAILS}
+`;
+
+const COMMENTS_SUBSCRIPTION = gql`
+  subscription OnTodoItemAdded {
+    todoItemAdded {
+      ...TodoItemDetails
+    }
+  }
+  ${TODOITEM_DETAILS}
 `;
 
 export default function TodoItems() {
-	const [modal, setModal] = useState(false);
-	const todoItems = useQuery(GET_TODOITEMS);
+  const [modal, setModal] = useState(false);
+  const { subscribeToMore, loading, error, data } = useQuery(GET_TODOITEMS);
+  const { todoItems } = data ?? [];
 
-	const [addTodoItem, newTodoItem] = useMutation(CREATE_TODOITEM, {
-		update(cache, { data: { addTodoItem } }) {
-			const { todoItems } = cache.readQuery({ query: GET_TODOITEMS });
+  useEffect(
+    subscribeToMore({
+      document: COMMENTS_SUBSCRIPTION,
 
-			cache.writeQuery({
-				query: GET_TODOITEMS,
-				data: { todoItems: [addTodoItem, ...todoItems] },
-			});
-		},
-	});
+      updateQuery: (prev, { subscriptionData }) => {
+        //todo why is this getting called 4+
+        console.log("subscriptionData");
+        // console.log(subscriptionData);
 
-	if (todoItems.loading) {
-		return  (<Loader />);
-	}
+        if (!subscriptionData.data) {
+          return; //was return prev in previous versions
+        }
 
-	if (todoItems.error || newTodoItem.error) {return (<p>ERROR</p>);}
+        const newTodoItem = subscriptionData.data.todoItemAdded;
 
-	const onSubmit = (input) => {
-		setModal(false);
-		addTodoItem({
-			variables: { input },
+        let moddedTodoItems = prev.todoItems;
 
-			optimisticResponse: {
-				__typename: "Mutation",
-				addTodoItem: {
-					__typename: "TodoItem",
-					id: 'Saving',
-					label: input.label,
-					isCompleted: input.isCompleted,
-				},
-			},
-		});
-	};
+        if (moddedTodoItems.some((item) => newTodoItem.id === item.id)) {
+          return;
+        }
 
-	const todoItemsList = todoItems.data.todoItems.map((todoItem) => (
-		<TodoItemBox key={`${todoItem.id}-${todoItem.label}`} todoItem={todoItem} />
-	));
+        return { todoItems: [newTodoItem, ...moddedTodoItems] }; //todo if this happens at same time as the subscription event duplicates occur
+      },
+    }),
+    []
+  );
 
-	if (modal) {
-		return <NewTodoItem onSubmit={onSubmit} onCancel={() => setModal(false)} />;
-	}
+  const [addTodoItem, newTodoItem] = useMutation(CREATE_TODOITEM, {
+    update(cache, { data: { addTodoItem } }) {
+      const { todoItems } = cache.readQuery({ query: GET_TODOITEMS });
 
-	return (
-		<div>
-			<section>
-				<h1>TodoItems</h1>
-				<button onClick={() => setModal(true)}>new todoItem</button>
-			</section>
-			<section>
-				<div>{todoItemsList}</div>
-			</section>
-		</div>
-	);
+      cache.writeQuery({
+        query: GET_TODOITEMS,
+        data: { todoItems: [addTodoItem, ...todoItems] }, //todo if this happens at same time as the subscription event duplicates occur
+      });
+    },
+  });
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error || newTodoItem.error) {
+    return <p>ERROR</p>;
+  }
+
+  const onSubmit = (input) => {
+    setModal(false);
+    addTodoItem({
+      variables: { input },
+
+      optimisticResponse: {
+        __typename: "Mutation",
+        addTodoItem: {
+          __typename: "TodoItem",
+          id: "Saving",
+          label: input.label,
+          isCompleted: input.isCompleted,
+        },
+      },
+    });
+  };
+
+  const todoItemsList = todoItems.map((todoItem) => (
+    <TodoItemBox key={`${todoItem.id}-${todoItem.label}`} todoItem={todoItem} />
+  ));
+
+  if (modal) {
+    return <NewTodoItem onSubmit={onSubmit} onCancel={() => setModal(false)} />;
+  }
+
+  return (
+    <div>
+      <section>
+        <h1>TodoItems</h1>
+        <button onClick={() => setModal(true)}>new todoItem</button>
+      </section>
+      <section>
+        <div>{todoItemsList}</div>
+      </section>
+    </div>
+  );
 }
