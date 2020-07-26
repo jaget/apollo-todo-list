338a40cc041dc2a09997cdc2ec5ddcc51f73c71d
jaget <juliantaylor14@googlemail.com>
2020-07-26 22:43:31 +0100

    white space changes and remove array wrapping on TODO_ITEM_ADDED

--- server/resolvers.js
+++ server/resolvers.js
@@ -5,32 +5,33 @@ const pubsub = new PubSub();
 const TODO_ITEM_ADDED = "TODO_ITEM_ADDED";
 
 module.exports = {
-	Query: {
-		todoLists: (_, { input }, { dataSources }) =>
-			dataSources.todoListsAPI.list({ ...input }),
-		todoItems: (_, { input }, { dataSources }) =>
-			dataSources.todoItemsAPI.list({ ...input }),
-		todoList: (_, { id }, { dataSources }) => dataSources.TodoList.find(id),
-		todoItem: (_, { input }, { dataSources }) =>
-			dataSources.todoItemsAPI.find(input.id),
-	},
-	Mutation: {
-		addTodoItem: (_, { input }, models) => {
-			const result = models.dataSources.todoItemsAPI.create({ ...input });
-			pubsub.publish(TODO_ITEM_ADDED, { todoItemAdded: result });
-			return result;
-		},
-		deleteTodoItem: (_, { input }, models) =>
-			models.dataSources.todoItemsAPI.delete({ ...input }),
-		updateTodoItem: (_, { input }, models) =>
-			models.dataSources.todoItemsAPI.update({ ...input }),
-		addTodoList: (_, { input }, { models }) =>
-			models.TodoList.create({ ...input }),
-	},
-	Subscription: {
-		todoItemAdded: {
-			// Additional event labels can be passed to asyncIterator creation
-			subscribe: () => pubsub.asyncIterator([TODO_ITEM_ADDED]),
-		},
-	},
+  Query: {
+    todoLists: (_, { input }, { dataSources }) =>
+      dataSources.todoListsAPI.list({ ...input }),
+    todoItems: (_, { input }, { dataSources }) =>
+      dataSources.todoItemsAPI.list({ ...input }),
+    todoList: (_, { id }, { dataSources }) => dataSources.TodoList.find(id),
+    todoItem: (_, { input }, { dataSources }) =>
+      dataSources.todoItemsAPI.find(input.id),
+  },
+  Mutation: {
+    addTodoItem: (_, { input }, models) => {
+      const result = models.dataSources.todoItemsAPI.create({ ...input });
+      pubsub.publish(TODO_ITEM_ADDED, { todoItemAdded: result });
+
+      return result;
+    },
+    deleteTodoItem: (_, { input }, models) =>
+      models.dataSources.todoItemsAPI.delete({ ...input }),
+    updateTodoItem: (_, { input }, models) =>
+      models.dataSources.todoItemsAPI.update({ ...input }),
+    addTodoList: (_, { input }, { models }) =>
+      models.TodoList.create({ ...input }),
+  },
+  Subscription: {
+    todoItemAdded: {
+      // Additional event labels can be passed to asyncIterator creation
+      subscribe: () => pubsub.asyncIterator(TODO_ITEM_ADDED),
+    },
+  },
 };
