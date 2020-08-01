92fe7d22029260a31370dc1e20f3654f088b47bd
jaget <juliantaylor14@googlemail.com>
2020-08-01 22:03:11 +0100

    Setup sequelize-cli generated models and migrations

---
+++ server/migrations/20200728223305-create-todo-items.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TodoItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      label: {
+        type: Sequelize.STRING
+      },
+      isCompleted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TodoItems');
+  }
+};
