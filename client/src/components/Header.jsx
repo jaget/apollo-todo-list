b8583c8fa00a49f13df95004e021e4520bf3001d
jaget <juliantaylor14@googlemail.com>
2020-10-21 08:26:58 +0100

    Include login component in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { withRouter } from "react-router";
+import {Link} from "react-router-dom";
+import {withRouter} from "react-router";
+import Login from "./Login";
 
 const Header = () => (
-	<header>
-		<Link to="/">Home</Link>
-	</header>
+    <header>
+        <Link to="/">Home</Link>
+        <Login/>
+    </header>
 );
 
 export default withRouter(Header);
