f34d339e7df12906fa9219205f4ff3fc6f969829
jaget <juliantaylor14@googlemail.com>
2020-11-28 22:44:54 +0000

    Update login request endpoint (resolver)

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,18 +1,21 @@
 import React, {useState} from "react";
 import gql from "graphql-tag";
 
-import { useQuery, useMutation } from "@apollo/react-hooks";
+import {useQuery, useMutation} from "@apollo/react-hooks";
 
+//todo cannot use login input as it is an "input" not a graphql "type"
 const LOGIN_DETAILS = gql`
-    fragment LoginDetails on SigninInput {
-        email
-        password
+    fragment LoginDetails on AuthUser {
+        token
+        user {
+            email
+        }
     }
 `;
 
 const ATTEMPT_SIGNIN = gql`
-    mutation AttemptSigning($input: SigninInput!) {
-        signIn(input: $input) {
+    mutation AttemptSignin($input: LoginInput!) {
+        login(input: $input) {
             ...LoginDetails
         }
     }
@@ -20,36 +23,51 @@ const ATTEMPT_SIGNIN = gql`
 `;
 
 
-
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState('');
 
-    const onSubmit = (e) => {
+    const [login, loginItem] = useMutation(ATTEMPT_SIGNIN, {
+        update(cache, {data: {login}}) {
+            const {loginItem} = cache.readQuery({query: ATTEMPT_SIGNIN});
+
+            cache.writeQuery({
+                query: ATTEMPT_SIGNIN,
+                data: {loginItem},
+            });
+        },
+    });
+
+
+    const submit = (e) => {
         e.preventDefault();
+        onSubmit({email: username, password});
+    }
 
-        signIn({
-            variables: {username, password},
+    const onSubmit = (input) => {
+        // e.preventDefault();
+        login({
+            variables: {input},
 
-            optimisticResponse: {
-                __typename: "Mutation",
-                signIn: {
-                    __typename: "SigninInput",
-                    email: username,
-                    password: password
-                },
-            },
+            // optimisticResponse: {
+            //     __typename: "Mutation",
+            //     signin: {
+            //         __typename: "SigninInput",
+            //         email: username,
+            //         password: password
+            //     },
+            // },
         });
     };
 
 
-    return (<form onSubmit={onSubmit}>
+    return (<form onSubmit={submit}>
             <label>
                 <input type="email" placeholder='Email address' value={username}
                        onChange={(e) => setUsername(e.target.value)}/>
             </label>
             <label>
-                <input type="password" placeholder='Enter password'  value={password}
+                <input type="password" placeholder='Enter password' value={password}
                        onChange={(e) => setPassword(e.target.value)}/>
             </label>
             <input type="submit" value='submit'/>
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,37 +1,66 @@
+const {AuthenticationError} = require('apollo-server')
 const {PubSub} = require('apollo-server');
+const {authenticated, authorized} = require('./auth')
 
 const pubsub = new PubSub();
 
 const TODO_ITEM_ADDED = "TODO_ITEM_ADDED";
 
 module.exports = {
-  Query: {
-    todoLists: (_, { input }, { dataSources }) =>
-      dataSources.todoListsAPI.list({ ...input }),
-    todoItems: (_, { input }, { dataSources }) =>
-      dataSources.todoItemsAPI.list({ ...input }),
-    todoList: (_, { id }, { dataSources }) => dataSources.TodoList.find(id),
-    todoItem: (_, { input }, { dataSources }) =>
-      dataSources.todoItemsAPI.find(input.id),
-  },
-  Mutation: {
-    addTodoItem: (_, { input }, models) => {
-      const result = models.dataSources.todoItemsAPI.create({ ...input });
-      pubsub.publish(TODO_ITEM_ADDED, { todoItemAdded: result });
+    Query: {
+        todoLists: (_, {input}, {dataSources}) =>
+            dataSources.todoListsAPI.list({...input}),
+        todoItems: (_, {input}, {dataSources}) =>
+            dataSources.todoItemsAPI.list({...input}),
+        todoList: (_, {id}, {dataSources}) => dataSources.TodoList.find(id),
+        todoItem: (_, {input}, {dataSources}) =>
+            dataSources.todoItemsAPI.find(input.id)
+    },
+    Mutation: {
+        addTodoItem: (_, {input}, models) => {
+            console.log('models is ', models);
+            const result = models.dataSources.todoItemsAPI.create({...input});
+            pubsub.publish(TODO_ITEM_ADDED, {todoItemAdded: result});
 
-      return result;
+            return result;
+        },
+        deleteTodoItem: (_, {input}, models) =>
+            models.dataSources.todoItemsAPI.delete({...input}),
+        updateTodoItem: (_, {input}, models) =>
+            models.dataSources.todoItemsAPI.update({...input}),
+        addTodoList: (_, {input}, {models}) =>
+            models.TodoList.create({...input}),
+        register: (_, {input}, {models, createToken}) => {
+            console.log('models is ', models);
+            const existing = models.User.findOne({email: input.email})
+
+            if (existing) {
+                throw new AuthenticationError('nope')
+            }
+            const user = models.User.createOne({...input, verified: false, avatar: 'http'})
+            const token = createToken(user)
+            models.Settings.createOne({user: user.id, theme: 'DARK', emailNotifications: true, pushNotifications: true})
+            return {token, user}
+        },
+        login: (_, {input}, {models, createToken}) => {
+
+            const user = models.User.findOne(input)
+
+            if (!user) {
+                throw new AuthenticationError('wrong email + password combo')
+            }
+
+            const token = createToken(user)
+            // return {token: 'token', user: {email: 'login resolver'}};
+
+            return {token, user}
+        }
+
     },
-    deleteTodoItem: (_, { input }, models) =>
-      models.dataSources.todoItemsAPI.delete({ ...input }),
-    updateTodoItem: (_, { input }, models) =>
-      models.dataSources.todoItemsAPI.update({ ...input }),
-    addTodoList: (_, { input }, { models }) =>
-      models.TodoList.create({ ...input }),
-  },
-  Subscription: {
-    todoItemAdded: {
-      // Additional event labels can be passed to asyncIterator creation
-      subscribe: () => pubsub.asyncIterator(TODO_ITEM_ADDED),
+    Subscription: {
+        todoItemAdded: {
+            // Additional event labels can be passed to asyncIterator creation
+            subscribe: () => pubsub.asyncIterator(TODO_ITEM_ADDED),
+        },
     },
-  },
 };
